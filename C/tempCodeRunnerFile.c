d8ee6486c423b6544b39e95c36f685b00926317b
Saikat Saha <saikatsaha798@gmail.com>
2023-05-10 17:46:11 +0530

    Prim Algorithm

--- C/tempCodeRunnerFile.c
+++ C/tempCodeRunnerFile.c
@@ -1,7 +1,2 @@
-for (k=0; k<n; k++){
-                if (k == n-1){
-                    printf("%d\n", arr[k]);
-                    break;
-                }
-                printf("%d, ", arr[k]);
-            }
+
+    while (n<v){
